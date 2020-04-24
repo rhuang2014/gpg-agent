86e69276383bb626232b4f8b5b93f99078929315
Roger Huang <rhuang@gmail.com>
2020-04-24 14:18:35 -0700

    Add support for other agent

--- gpg-agent.zsh
+++ gpg-agent.zsh
@@ -4,9 +4,12 @@
 #
 # version 1.0.0
 # author: Roger huang
-# url: https://github.com/rhuang2014/gpg-agent
+# url: https://github.com/rhuang/gpg-agent
 
-(( $+commands[gpg-agent] )) || return
+# run the below command to set a different gpg-agent. i.e. YubiKey Smart Card agent
+# if ! egrep -q '^[^#].*GPG_AGENT=' ~/.zshenv; then echo 'export GPG_AGENT="otheragent"' >> ~/.zshenv; fi
+typeset -g GPG_AGENT="${GPG_AGENT:-gpg}-agent"
+(( $+commands[${GPG_AGENT}] )) || return 1
 typeset -g SSH_AUTH_SOCK AGENT_SOCK
 
 gpg-agent-start() {
@@ -19,13 +22,18 @@ gpg-agent-stop() {
     fi
 }
 
+other-agent-ssh() {
+    export SSH_AUTH_SOCK=$(launchctl getenv SSH_AUTH_SOCK)
+}
+
 gpg-agent-ssh() {
     GNUPGCONFIG="${GNUPGHOME:-"${HOME}/.gnupg"}/gpg-agent.conf"
     [[ -r "${GNUPGCONFIG}" ]] &&\
-        if command grep -q 'enable-ssh-support' "${GNUPGCONFIG}"; then
+        if command grep -q '^enable-ssh-support' "${GNUPGCONFIG}"; then
             export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
             unset SSH_AGENT_PID
         fi
+        # update GPG-Agent TTY
         gpg-connect-agent updatestartuptty /bye &>/dev/null
 }
 
@@ -37,17 +45,30 @@ gpg-agent-pinentry() {
 }
 
 gpg-agent-status() {
-    command gpg-agent --version
-    echo -e "\nPID: ${$(pidof gpg-agent):-NOT_FOUND}, SOCK: ${SSH_AUTH_SOCK}\n"
+    [[ ${GPG_AGENT} != "gpg-agent" ]] && other-agent-ssh
+    command ${GPG_AGENT} --version >/dev/null || command ${GPG_AGENT} -version
+    echo -e "\nPID: ${${$(pidof ${GPG_AGENT}):gs/ //}:-NOT_FOUND}, SOCK: ${SSH_AUTH_SOCK}\n"
     gpgconf --list-components
+    [[ -n ${SSH_AUTH_SOCK} ]] && [[ ${GPG_AGENT} != "gpg-agent" ]] &&\
+        eval ${${(s_-_)GPG_AGENT}[1]} stats
+    return 0
 }
 
 gpg-agent-init() {
-    AGENT_SOCK="$(gpgconf --list-dirs agent-socket)"
-    if [[ ! -S ${AGENT_SOCK} ]]; then
-        gpgconf --launch gpg-agent &>/dev/null
+    if [[ ${GPG_AGENT} != "gpg-agent" ]]; then
+        other-agent-ssh
+    else
+        AGENT_SOCK="$(gpgconf --list-dirs agent-socket)"
+        if [[ ! -S ${AGENT_SOCK} ]]; then
+            gpgconf --launch gpg-agent &>/dev/null
+        fi
     fi
+    # Set GPG_TTY so gpg-agent knows where to prompt.  See gpg-agent(1)
     export GPG_TTY="${TTY}"
+    if [[ -n "$SSH_CONNECTION" ]]; then
+        # Set PINENTRY_USER_DATA so pinentry-auto knows to present a text UI.
+        export PINENTRY_USER_DATA='USE_CURSES=1'
+    fi
     gpg-agent-ssh
 }
 
@@ -64,5 +85,5 @@ gpg-agent() {
     done
     gpg-agent-init
 }
-
 gpg-agent "$@"
+return 0
